44be5c31667033c6ccbb1eccf03e2ba0509abf3c
idiot <32636970+ostepizza@users.noreply.github.com>
2022-10-13 10:49:43 +0200

    attempted to add foreign keys

--- ndtables.sql
+++ ndtables.sql
@@ -1,87 +1,103 @@
--- LACKS FOREIGN KEYS
+-- Sets up the database to use
+create database if not exists webAppDatabase;
+use webAppDatabase;
 
 -- Create table for employees
-CREATE TABLE `employee` (
-    `emp_nr` INT(11) NOT NULL ,
-    `emp_fname` VARCHAR(255) NOT NULL ,
-    `emp_lname` VARCHAR(255) NOT NULL ,
-    `emp_phone` VARCHAR(30) NULL ,
-    `emp_email` VARCHAR(255) NULL ,
-    `team_id` INT(11) NULL ,
-    `emp_pword` VARCHAR(255) NOT NULL ,
+CREATE TABLE `employees` (
+    `emp_nr` INT(11) NOT NULL,
+    `emp_fname` VARCHAR(255) NOT NULL,
+    `emp_lname` VARCHAR(255) NOT NULL,
+    `emp_phone` VARCHAR(30) NULL,
+    `emp_email` VARCHAR(255) NULL,
+    `team_id` INT(11) NULL,
+    `emp_pword` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`emp_nr`),
     UNIQUE (`emp_email`)
 );
 
 -- Create table for teams
-CREATE TABLE `team` (
-    `team_id` INT(11) NOT NULL AUTO_INCREMENT ,
-    `team_name` VARCHAR(255) NOT NULL ,
-    `leader_emp_nr` INT(11) NULL ,
+CREATE TABLE `teams` (
+    `team_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `team_name` VARCHAR(255) NOT NULL,
+    `leader_emp_nr` INT(11) NULL,
     PRIMARY KEY (`team_id`),
-    UNIQUE (`team_name`)
+    UNIQUE (`team_name`),
+    FOREIGN KEY (`leader_emp_nr`) REFERENCES employees(`emp_nr`)
 );
+
+-- Adds foreign key between employees and teams
+ALTER TABLE `employees`
+ADD FOREIGN KEY (`team_id`) REFERENCES teams(`team_id`);
 
 -- Create table for superusers
 CREATE TABLE `superusers` (
-    `su_id` INT(11) NOT NULL AUTO_INCREMENT ,
-    `emp_nr` INT(11) NOT NULL ,
+    `su_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `emp_nr` INT(11) NOT NULL,
     PRIMARY KEY (`su_id`),
-    UNIQUE (`emp_nr`)
+    UNIQUE (`emp_nr`),
+    FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`)
 );
 
 -- Create table for statuses
 CREATE TABLE `status` (
-    `status_id` INT(11) NOT NULL AUTO_INCREMENT ,
-    `status_title` VARCHAR(11) NOT NULL ,
+    `status_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `status_title` VARCHAR(11) NOT NULL,
     PRIMARY KEY (`status_id`),
     UNIQUE (`status_title`)
 );
 
 -- Create table for suggestions
-CREATE TABLE `suggestion` (
-    `suggestion_id` INT(11) NOT NULL AUTO_INCREMENT ,
-    `suggestion_title` VARCHAR(255) NOT NULL ,
-    `suggestion_description` VARCHAR(2000) NOT NULL ,
-    `suggestion_deadline` DATE NOT NULL ,
-    `suggestion_enddate` DATE NULL ,
-    `status_id` INT(11) NOT NULL ,
-    `suggested_emp_nr` INT(11) NOT NULL ,
-    `responsible_emp_nr` INT(11) NOT NULL ,
+CREATE TABLE `suggestions` (
+    `suggestion_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `suggestion_title` VARCHAR(255) NOT NULL,
+    `suggestion_description` VARCHAR(2000) NOT NULL,
+    `suggestion_deadline` DATE NOT NULL,
+    `suggestion_enddate` DATE NULL,
+    `status_id` INT(11) NOT NULL,
+    `suggested_emp_nr` INT(11) NOT NULL,
+    `responsible_emp_nr` INT(11) NOT NULL,
     `team_id` INT(11) NOT NULL ,
-    PRIMARY KEY (`suggestion_id`)
+    PRIMARY KEY (`suggestion_id`),
+    FOREIGN KEY (`status_id`) REFERENCES status(`status_id`),
+    FOREIGN KEY (`suggested_emp_nr`) REFERENCES employees(`emp_nr`),
+    FOREIGN KEY (`responsible_emp_nr`) REFERENCES employees(`emp_nr`),
+    FOREIGN KEY (`team_id`) REFERENCES teams(`team_id`)
 );
 
 -- Create table for repairs
 CREATE TABLE `repairs` (
-    `repairs_id` INT(11) NOT NULL AUTO_INCREMENT ,
-    `repairs_title` VARCHAR(255) NOT NULL ,
-    `repairs_description` VARCHAR(2000) NOT NULL ,
-    `repairs_deadline` DATE NOT NULL ,
-    `repairs_enddate` DATE NULL ,
-    `repairs_cost` VARCHAR(255) NULL ,
-    `status_id` INT(11) NOT NULL ,
-    `emp_nr` INT(11) NOT NULL ,
-    `team_id` INT(11) NOT NULL ,
-    PRIMARY KEY (`repairs_id`)
+    `repairs_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `repairs_title` VARCHAR(255) NOT NULL,
+    `repairs_description` VARCHAR(2000) NOT NULL,
+    `repairs_deadline` DATE NOT NULL,
+    `repairs_enddate` DATE NULL,
+    `repairs_cost` VARCHAR(255) NULL,
+    `status_id` INT(11) NOT NULL,
+    `emp_nr` INT(11) NOT NULL,
+    `team_id` INT(11) NOT NULL,
+    PRIMARY KEY (`repairs_id`),
+    FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`),
+    FOREIGN KEY (`team_id`) REFERENCES teams(`team_id`)
 );
 
 -- Create table for commenting on suggestions
 CREATE TABLE `commentssug` ( 
-    `commentsug_id` INT(11) NOT NULL AUTO_INCREMENT , 
-    `suggestion_id` INT(11) NOT NULL , 
-    `commentsug_time` TIMESTAMP NOT NULL , 
-    `commentsug_text` VARCHAR(255) NOT NULL , 
-    `emp_id` INT(11) NOT NULL , 
-    PRIMARY KEY (`commentsug_id`)
+    `commentsug_id` INT(11) NOT NULL AUTO_INCREMENT, 
+    `suggestion_id` INT(11) NOT NULL, 
+    `commentsug_time` TIMESTAMP NOT NULL, 
+    `commentsug_text` VARCHAR(255) NOT NULL, 
+    `emp_id` INT(11) NOT NULL, 
+    PRIMARY KEY (`commentsug_id`),
+    FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`)
 );
 
 -- Create table for commenting on repairs
 CREATE TABLE `commentsrep` (
-    `commentrep_id` INT(11) NOT NULL AUTO_INCREMENT , 
-    `repairs_id` INT(11) NOT NULL , 
-    `commentrep_time` TIMESTAMP NOT NULL , 
-    `commentrep_text` VARCHAR(255) NOT NULL , 
-    `emp_id` INT(11) NOT NULL , 
-    PRIMARY KEY (`commentrep_id`)
+    `commentrep_id` INT(11) NOT NULL AUTO_INCREMENT, 
+    `repairs_id` INT(11) NOT NULL, 
+    `commentrep_time` TIMESTAMP NOT NULL, 
+    `commentrep_text` VARCHAR(255) NOT NULL, 
+    `emp_id` INT(11) NOT NULL, 
+    PRIMARY KEY (`commentrep_id`),
+    FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`)
 );
