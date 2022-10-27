c8eb5ea3bff62d0dee6085a552d4f9a87bc5d77c
idiot <32636970+ostepizza@users.noreply.github.com>
2022-10-27 13:27:24 +0200

    More example SQL + minor changes

--- ndtables.sql
+++ ndtables.sql
@@ -81,22 +81,22 @@ CREATE TABLE `repairs` (
 );
 
 -- Create table for commenting on suggestions
-CREATE TABLE `commentssug` ( 
+CREATE TABLE `commentsug` ( 
     `commentsug_id` INT(11) NOT NULL AUTO_INCREMENT, 
     `suggestion_id` INT(11) NOT NULL, 
     `commentsug_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(), 
-    `commentsug_text` VARCHAR(255) NOT NULL, 
+    `commentsug_text` VARCHAR(500) NOT NULL, 
     `emp_nr` INT(11) NOT NULL, 
     PRIMARY KEY (`commentsug_id`),
     FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`)
 );
 
 -- Create table for commenting on repairs
-CREATE TABLE `commentsrep` (
+CREATE TABLE `commentrep` (
     `commentrep_id` INT(11) NOT NULL AUTO_INCREMENT, 
     `repairs_id` INT(11) NOT NULL, 
     `commentrep_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(), 
-    `commentrep_text` VARCHAR(255) NOT NULL, 
+    `commentrep_text` VARCHAR(500) NOT NULL, 
     `emp_nr` INT(11) NOT NULL, 
     PRIMARY KEY (`commentrep_id`),
     FOREIGN KEY (`emp_nr`) REFERENCES employees(`emp_nr`)
@@ -153,40 +153,82 @@ UPDATE `status` SET `status_id` = 4 WHERE `status_title` = 'Fullført';
 INSERT INTO `suggestions` (`suggestion_id`, `suggestion_title`, `suggestion_description`, `suggestion_deadline`, `suggestion_enddate`, `status_id`, `suggested_emp_nr`, `responsible_emp_nr`, `team_id`)
 SELECT NULL, 'Cola kjøleskap hos IT', 'Det er tungt arbeid å jobbe med PC. Derfor synes jeg at vi i IT burde få oss et helt eget Cola-themed kjøleskap nede hos oss.', '2023-01-28', NULL, 0, '000', '001', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '000';
+UPDATE `suggestions` SET `suggestion_id` = 0 WHERE `suggestion_title` = 'Cola kjøleskap hos IT';
 
 INSERT INTO `suggestions` (`suggestion_id`, `suggestion_title`, `suggestion_description`, `suggestion_deadline`, `suggestion_enddate`, `status_id`, `suggested_emp_nr`, `responsible_emp_nr`, `team_id`)
 SELECT NULL, 'Lås opp døra', 'Vi er lei av å være innelåst i kjelleren!! Få oss ut!!!', '2022-12-24', NULL, 0, '002', '001', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '002';
+UPDATE `suggestions` SET `suggestion_id` = 1 WHERE `suggestion_title` = 'Lås opp døra';
 
 INSERT INTO `suggestions` (`suggestion_id`, `suggestion_title`, `suggestion_description`, `suggestion_deadline`, `suggestion_enddate`, `status_id`, `suggested_emp_nr`, `responsible_emp_nr`, `team_id`)
 SELECT NULL, 'Nye SSDer', 'PC slow need harder drives', '2023-06-01', NULL, 0, '000', '002', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '000';
+UPDATE `suggestions` SET `suggestion_id` = 2 WHERE `suggestion_title` = 'Nye SSDer';
 
 INSERT INTO `suggestions` (`suggestion_id`, `suggestion_title`, `suggestion_description`, `suggestion_deadline`, `suggestion_enddate`, `status_id`, `suggested_emp_nr`, `responsible_emp_nr`, `team_id`)
 SELECT NULL, 'Ny maskin', 'Vi trenger en ny maskin', '2023-04-14', NULL, 0, '004', '001', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '004';
+UPDATE `suggestions` SET `suggestion_id` = 3 WHERE `suggestion_title` = 'Ny maskin';
 
 INSERT INTO `suggestions` (`suggestion_id`, `suggestion_title`, `suggestion_description`, `suggestion_deadline`, `suggestion_enddate`, `status_id`, `suggested_emp_nr`, `responsible_emp_nr`, `team_id`)
 SELECT NULL, 'Ny kaffetrakter', 'Blir for lite kaffe i produksjonshallen. Vi trenger en ny ein!', '2023-01-28', NULL, 0, '005', '002', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '005';
+UPDATE `suggestions` SET `suggestion_id` = 4 WHERE `suggestion_title` = 'Ny kaffetrakter';
 
 -- Adding repairs
 INSERT INTO `repairs` (`repairs_id`, `repairs_title`, `repairs_description`, `repairs_deadline`, `repairs_enddate`, `repairs_cost`, `status_id`, `emp_nr`, `team_id`)
 SELECT NULL, 'Ødelagt PSU', 'En maskin er nede pga. ødelagt PSU.', '2022-12-15', NULL, 450, 0, '000', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '000';
+UPDATE `repairs` SET `repairs_id` = 0 WHERE `repairs_title` = 'Ødelagt PSU';
 
 INSERT INTO `repairs` (`repairs_id`, `repairs_title`, `repairs_description`, `repairs_deadline`, `repairs_enddate`, `repairs_cost`, `status_id`, `emp_nr`, `team_id`)
 SELECT NULL, 'Ødelagt tannhjul', 'Maskinen mista et tannhjul og er ute av drift', '2022-12-10', NULL, 1250, 0, '005', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '005';
+UPDATE `repairs` SET `repairs_id` = 1 WHERE `repairs_title` = 'Ødelagt tannhjul';
 
 INSERT INTO `repairs` (`repairs_id`, `repairs_title`, `repairs_description`, `repairs_deadline`, `repairs_enddate`, `repairs_cost`, `status_id`, `emp_nr`, `team_id`)
 SELECT NULL, 'Verktøy gikk i stykker', 'En sag og to skrujern gikk i stykker', '2022-11-30', NULL, 650, 0, '004', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '004';
+UPDATE `repairs` SET `repairs_id` = 2 WHERE `repairs_title` = 'Verktøy gikk i stykker';
 
 INSERT INTO `repairs` (`repairs_id`, `repairs_title`, `repairs_description`, `repairs_deadline`, `repairs_enddate`, `repairs_cost`, `status_id`, `emp_nr`, `team_id`)
 SELECT NULL, 'Ødelagt sandebånd', 'Sandebåndet er for glatt og må byttes', '2022-12-30', NULL, 2000, 0, '005', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '005';
+UPDATE `repairs` SET `repairs_id` = 3 WHERE `repairs_title` = 'Ødelagt sandebånd';
 
 INSERT INTO `repairs` (`repairs_id`, `repairs_title`, `repairs_description`, `repairs_deadline`, `repairs_enddate`, `repairs_cost`, `status_id`, `emp_nr`, `team_id`)
 SELECT NULL, 'Tett malesprøyte', 'Malesprøyten er tett og må dyprenses', '2022-12-5', NULL, 200, 0, '004', `employees`.`team_id`
 FROM `employees` WHERE `employees`.`emp_nr` = '004';
+UPDATE `repairs` SET `repairs_id` = 4 WHERE `repairs_title` = 'Tett malesprøyte';
+
+-- Adding suggestion comments
+INSERT INTO `commentsug` (`commentsug_id`, `suggestion_id`, `commentsug_time`, `commentsug_text`, `emp_nr`)
+VALUES (NULL, 0, CURRENT_DATETIME(), 'Bra forslag! Cola er godt!', '001');
+
+INSERT INTO `commentsug` (`commentsug_id`, `suggestion_id`, `commentsug_time`, `commentsug_text`, `emp_nr`)
+VALUES (NULL, 1, CURRENT_DATETIME(), 'Elendig forslag...', '001');
+
+INSERT INTO `commentsug` (`commentsug_id`, `suggestion_id`, `commentsug_time`, `commentsug_text`, `emp_nr`)
+VALUES (NULL, 2, CURRENT_DATETIME(), 'Det høres bra ut.', '001');
+
+INSERT INTO `commentsug` (`commentsug_id`, `suggestion_id`, `commentsug_time`, `commentsug_text`, `emp_nr`)
+VALUES (NULL, 3, CURRENT_DATETIME(), 'Oi, dette hørtes dyrt ut!!', '001');
+
+INSERT INTO `commentsug` (`commentsug_id`, `suggestion_id`, `commentsug_time`, `commentsug_text`, `emp_nr`)
+VALUES (NULL, 4, CURRENT_DATETIME(), 'KOFFEIN :DDDD', '001');
+
+-- Adding repair comments
+INSERT INTO `commentrep` (`commentrep_id`, `repairs_id`, `commentrep_time`, `commentrep_text`, `emp_nr`)
+VALUES (NULL, 0, CURRENT_DATETIME(), 'Viktig å få fiksa!', '001');
+
+INSERT INTO `commentrep` (`commentrep_id`, `repairs_id`, `commentrep_time`, `commentrep_text`, `emp_nr`)
+VALUES (NULL, 1, CURRENT_DATETIME(), 'Bra å få ordna!', '001');
+
+INSERT INTO `commentrep` (`commentrep_id`, `repairs_id`, `commentrep_time`, `commentrep_text`, `emp_nr`)
+VALUES (NULL, 2, CURRENT_DATETIME(), 'Hvordan klarte du å ødelegge en sag???', '001');
+
+INSERT INTO `commentrep` (`commentrep_id`, `repairs_id`, `commentrep_time`, `commentrep_text`, `emp_nr`)
+VALUES (NULL, 3, CURRENT_DATETIME(), 'Helt enig!', '001');
+
+INSERT INTO `commentrep` (`commentrep_id`, `repairs_id`, `commentrep_time`, `commentrep_text`, `emp_nr`)
+VALUES (NULL, 4, CURRENT_DATETIME(), 'Deilig med billige reparasjoner :D', '001');
