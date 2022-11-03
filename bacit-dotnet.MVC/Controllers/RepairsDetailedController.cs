6709bcb2d47fad2da50f90808455c9b027c74e99
Meepception <marius.e1998@gmail.com>
2022-11-03 14:22:05 +0100

    Repairs detaljert lastet opp på MVC

---
+++ bacit-dotnet.MVC/Controllers/RepairsDetailedController.cs
@@ -0,0 +1,13 @@
+using bacit_dotnet.MVC.Models.RepairsDetailed;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bacit_dotnet.MVC.Controllers
+{
+    public class RepairsDetailedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
