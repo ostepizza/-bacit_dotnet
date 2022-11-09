24ebf8d511d8af1d55c442dfb41295eda03efb95
AdamPajestka <adampp@uia.no>
2022-11-09 13:54:45 +0100

    legg til og rediger bruker, lagt til

---
+++ bacit-dotnet.MVC/Controllers/LeggTilBrukerController.cs
@@ -0,0 +1,14 @@
+﻿using bacit_dotnet.MVC.Models.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bacit_dotnet.MVC.Controllers
+{
+    public class LeggTilBrukerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
