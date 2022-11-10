8d3b5d9520d1df53f1b35dec17a11d0ffb01d96e
mnilsen <32636970+ostepizza@users.noreply.github.com>
2022-11-10 12:10:05 +0100

    Små ting på login

--- bacit-dotnet.MVC/Models/Login/LoginViewModel.cs
+++ bacit-dotnet.MVC/Models/Login/LoginViewModel.cs
@@ -1,10 +1,19 @@
 using bacit_dotnet.MVC.Entities;
 using bacit_dotnet.MVC.Repositories;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace bacit_dotnet.MVC.Models.Login
 {
     public class LoginViewModel
     {
+        [Required]
+        [Display(Name = "Ansattnummer")]
+        public string employeeNr { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passord")]
+        public string password { get; set; }
     }
 }
